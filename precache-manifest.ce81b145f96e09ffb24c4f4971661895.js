3facb43e4412d035cb436246f2841ef29a1bca3f
Anna Zhang <annazhang2002@gmail.com>
2020-09-03 13:29:38 -0700

    Updates

---
+++ precache-manifest.ce81b145f96e09ffb24c4f4971661895.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43712ee14f3bc672dff9b8bc9bee1f6a",
+    "url": "/twc-website/index.html"
+  },
+  {
+    "revision": "8eaf00bbf909d782e3e0",
+    "url": "/twc-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ab0ae45126c23b5bb7f0",
+    "url": "/twc-website/static/css/main.3e7bcca9.chunk.css"
+  },
+  {
+    "revision": "8eaf00bbf909d782e3e0",
+    "url": "/twc-website/static/js/2.dce27daa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/twc-website/static/js/2.dce27daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0ae45126c23b5bb7f0",
+    "url": "/twc-website/static/js/main.f42397c8.chunk.js"
+  },
+  {
+    "revision": "5212d99b99249d6e4eab",
+    "url": "/twc-website/static/js/runtime-main.1f2a73a1.js"
+  },
+  {
+    "revision": "2d1068a7f51e1fc2c63d81165bf52422",
+    "url": "/twc-website/static/media/arial-rounded-mt-bold.2d1068a7.ttf"
+  },
+  {
+    "revision": "488f96711b8e8670e1ab709ea5e06c2e",
+    "url": "/twc-website/static/media/heroimage.488f9671.png"
+  },
+  {
+    "revision": "9b20084c78e7fe49ecddc4e7dd7cdf63",
+    "url": "/twc-website/static/media/joinChapterImg.9b20084c.png"
+  },
+  {
+    "revision": "79fa593019c2d13e9ef29f6ebd496194",
+    "url": "/twc-website/static/media/logo.79fa5930.png"
+  },
+  {
+    "revision": "097d09403aabf3603c5f461ca5cc1c1a",
+    "url": "/twc-website/static/media/restaurantIcon.097d0940.png"
+  },
+  {
+    "revision": "5c3e2b4d7371a2bd374ad8a388450ada",
+    "url": "/twc-website/static/media/restaurantImg.5c3e2b4d.png"
+  },
+  {
+    "revision": "4bdb4f7f70858aeafb4f8ec917b4a6ce",
+    "url": "/twc-website/static/media/startChapterImg.4bdb4f7f.png"
+  }
+]);
--- src/assets/colors.js
+++
@@ -1,6 +0,0 @@
-export default {
-    white: '#fff',
-    black: '#000',
-    lightGreen: '#EAF8B8',
-    darkBlue: '#B2E4D6'
-}
