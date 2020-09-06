35d42e17f339b946b204800bf1f4fca5353a6ac4
Anna Zhang <annazhang2002@gmail.com>
2020-09-06 16:23:21 -0700

    Added about page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './assets/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => (
   <div className="center-text">
     <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
   </div>
 )
 
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/style.css';
+import Colors from '../assets/colors'
+
+import Header from '../components/Header'
+
+const About = () => (
+    <div>
+        <Header solid={false} />
+    </div>
+)
+
+export default About
